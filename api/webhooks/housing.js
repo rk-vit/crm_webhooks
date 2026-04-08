a561800865dfdcd3ed6f454521b94a29cdf59968
Revanth Kanna M <revanthkannam05@gmail.com>
2026-04-08 18:49:09 +0530

    Change lead ID filter from 'RS%' to 'AX%'

--- api/webhooks/housing.js
+++ api/webhooks/housing.js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
     } else {
       const lastLeads = await sql`
         SELECT id FROM leads 
-        WHERE id LIKE 'RS%' 
+        WHERE id LIKE 'AX%' 
         ORDER BY id DESC LIMIT 1
       `;
 
